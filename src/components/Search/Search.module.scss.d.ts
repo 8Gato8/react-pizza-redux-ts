60a6670479672dd295846cc442990dd5b7794a64
Sergey Skotselyas <sergey.sckotcelias@yandex.ru>
2024-02-20 21:38:26 +0800

    feat: Make svg and scss compatible with ts

---
+++ src/components/Search/Search.module.scss.d.ts
@@ -0,0 +1,4 @@
+export declare const clearIcon: string;
+export declare const input: string;
+export declare const root: string;
+export declare const searchIcon: string;
---
+++ src/scss/components/_sort.scss.d.ts
@@ -0,0 +1,4 @@
+export declare const active: string;
+export declare const sort: string;
+export declare const sortLabel: string;
+export declare const sortPopup: string;
