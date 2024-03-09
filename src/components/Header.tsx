62a0e26a0a9f23ca5f140938e5f6058b6550c060
Sergey Skotselyas <sergey.sckotcelias@yandex.ru>
2024-03-09 21:12:25 +0800

    feat: Make CartItem and Cart responsive

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,25 +1,11 @@
-import { useEffect, useRef } from 'react';
-
 import { useLocation } from 'react-router-dom';
 
 import { Search } from './Search';
-
-import { useAppSelector } from '../app/hooks';
-
-import { selectCart } from '../features/cart/cartSlice';
-import { selectFiltration } from '../features/filtration/filtrationSlice';
-import { selectPizzas } from '../features/pizzas/pizzasSlice';
 
 import { ReactPizzaLogo, CartLink } from './reexports';
 
 export const Header: React.FC = () => {
   const { pathname } = useLocation();
-
-  const cart = useAppSelector(selectCart);
-  const filtration = useAppSelector(selectFiltration);
-  const pizzas = useAppSelector(selectPizzas);
-
-  const isMounted = useRef(false);
 
   const renderContent = () => {
     if (pathname !== '/cart') {
@@ -31,30 +17,6 @@ export const Header: React.FC = () => {
       );
     }
   };
-
-  useEffect(() => {
-    if (isMounted.current) {
-      localStorage.setItem('cart', JSON.stringify(cart));
-    }
-
-    isMounted.current = true;
-  }, [cart]);
-
-  useEffect(() => {
-    if (isMounted.current) {
-      localStorage.setItem('filtration', JSON.stringify(filtration));
-    }
-
-    isMounted.current = true;
-  }, [filtration]);
-
-  useEffect(() => {
-    if (isMounted.current) {
-      localStorage.setItem('pizzas', JSON.stringify(pizzas));
-    }
-
-    isMounted.current = true;
-  }, [pizzas]);
 
   return (
     <section className="header">
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,5 +1,13 @@
 import '../scss/app.scss';
 
+import { useEffect, useRef } from 'react';
+
+import { useAppSelector } from '../app/hooks';
+
+import { selectCart } from '../features/cart/cartSlice';
+import { selectFiltration } from '../features/filtration/filtrationSlice';
+import { selectPizzas } from '../features/pizzas/pizzasSlice';
+
 import { Routes, Route } from 'react-router-dom';
 
 import { Header } from '../components/Header';
@@ -7,6 +15,36 @@ import { Header } from '../components/Header';
 import { Home, NotFound, Cart, SinglePizza } from './reexports';
 
 const App: React.FC = () => {
+  const cart = useAppSelector(selectCart);
+  const filtration = useAppSelector(selectFiltration);
+  const pizzas = useAppSelector(selectPizzas);
+
+  const isMounted = useRef(false);
+
+  useEffect(() => {
+    if (isMounted.current) {
+      localStorage.setItem('cart', JSON.stringify(cart));
+    }
+
+    isMounted.current = true;
+  }, [cart]);
+
+  useEffect(() => {
+    if (isMounted.current) {
+      localStorage.setItem('filtration', JSON.stringify(filtration));
+    }
+
+    isMounted.current = true;
+  }, [filtration]);
+
+  useEffect(() => {
+    if (isMounted.current) {
+      localStorage.setItem('pizzas', JSON.stringify(pizzas));
+    }
+
+    isMounted.current = true;
+  }, [pizzas]);
+
   return (
     <div className="App">
       <div className="wrapper">
