f72c7f0456cf9be0a479618e2c18ff4eb696483f
Sergey Skotselyas <sergey.sckotcelias@yandex.ru>
2024-03-15 21:30:47 +0800

    style: Change SkeletonForEmptyCart position and size

--- src/components/SkeletonForEmptyCart.tsx
+++ src/components/SkeletonForEmptyCart.tsx
@@ -8,9 +8,9 @@ export const SkeletonForEmptyCart: React.FC = () => (
     viewBox="0 0 514 790"
     backgroundColor="#f3f3f3"
     foregroundColor="#ecebeb">
-    <rect x="50" y="15" rx="10" ry="10" width="400" height="40" />
-    <rect x="0" y="95" rx="8" ry="8" width="790" height="80" />
-    <rect x="30" y="220" rx="15" ry="15" width="455" height="400" />
-    <rect x="105" y="710" rx="30" ry="30" width="325" height="80" />
+    <rect x="60" y="10" rx="10" ry="10" width="400" height="40" />
+    <rect x="10" y="90" rx="8" ry="8" width="790" height="80" />
+    <rect x="40" y="220" rx="15" ry="15" width="455" height="400" />
+    <rect x="105" y="710" rx="40" ry="40" width="350" height="80" />
   </ContentLoader>
 );
